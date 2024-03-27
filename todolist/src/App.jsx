6e1182b79b8f57c17d424017b87f31f79d0bcc40
Daniel Ahlberg <141239673+Poes93@users.noreply.github.com>
2024-03-27 18:14:57 +0100

    update app.jsx with app func

--- todolist/src/App.jsx
+++ todolist/src/App.jsx
@@ -1,1 +1,35 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+import TodoForm from './TodoForm';
+import TodoList from './TodoList';
+import "./App.css";
+
+function App() {
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        document.title = `${todos.filter(todo => !todo.isCompleted).length} tasks remaining`;
+    }, [todos]);
+
+    const addTodo = (text) => {
+        const newTodos = [...todos, { text, isCompleted: false }];
+        setTodos(newTodos);
+    };
+
+    const toggleTodo = (index) => {
+        const newTodos = [...todos];
+        newTodos[index].isCompleted = !newTodos[index].isCompleted;
+        setTodos(newTodos);
+    };
+
+    return (
+      <div className="app">
+          <div className="header">To-Do List Maker</div>
+          <TodoForm addTodo={addTodo} />
+          <TodoList todos={todos} toggleTodo={toggleTodo} />
+      </div>
+  );
+}
+
+
+
+export default App;
